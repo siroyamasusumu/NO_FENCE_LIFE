657c8cb2bbdd6011c3a0dde87c18b7e0cc52ddc7
siroyamasusumu <134294714+siroyamasusumu@users.noreply.github.com>
2024-02-21 13:50:50 +0900

    ハンバーガーメニューの設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// ハンバーガーメニュー
+document.querySelector('.hamburger').addEventListener('click', function(){
+  this.classList.toggle('active');
+  document.querySelector('.slide-menu').classList.toggle('active');
+})
